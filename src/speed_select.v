7f53cde4f9bbe7e0ee81e315597c52582334bdff
David Michael Indraputra <144904094+MikejR2904@users.noreply.github.com>
2024-11-05 22:39:57 +0800

    Create speed_select.v

---
+++ src/speed_select.v
@@ -0,0 +1,24 @@
+module speed_select(input sysclk, reset, en, power_up_active, curse_active, input [10:0] energy, output reg velocity_clk = 0);
+    reg [31:0] count = 0;
+    reg [31:0] max_count = 99_999_999;
+    localparam FAST_SPEED = 2_272_726; //22Hz
+    localparam NORM_SPEED = 2_777_776; //18Hz 
+    localparam NORM_SPEED_SLOW = 3_333_332; //15Hz
+    
+    always @ (posedge sysclk) begin
+        if (reset) begin
+            velocity_clk <= 0;
+            max_count <= NORM_SPEED / 3; //Factor of 3 is because the movement control is tri-state
+        end else if (en) begin
+            if (power_up_active) begin
+                max_count <= FAST_SPEED / 3;
+            end else if (!power_up_active && curse_active) begin
+                max_count <= (energy > 3) ? NORM_SPEED * 5 / 12 : NORM_SPEED_SLOW * 5 / 12;
+            end else begin
+                max_count <= (energy > 3) ? NORM_SPEED / 3 : NORM_SPEED_SLOW / 3;
+            end
+            count <= (count == max_count) ? 0 : count + 1;
+            velocity_clk <= (count == 0) ? ~velocity_clk : velocity_clk;
+        end
+    end
+endmodule
